41f4778f2af2e7f681e604d80d2b37972caf5124
SH-L <you@example.com>
2022-07-08 20:15:48 +0600

    dz1

---
+++ Lesoon_1/dz/greeting.js
@@ -0,0 +1,34 @@
+const name = prompt ("Как вас зовут?");
+console.log("Привет_" + name)
+
+//******** */
+
+const ar1 = ["1", "2", "3",4, 6];
+const ar2 = ["4", "3", "2", 1, 3, 6, 7, 6];
+if (ar1.length > ar2.length){
+    console.log('Первый массив длинее')
+} else if (ar1.length < ar2.length){
+    console.log('Вторый массив длинее')
+} else {
+
+}
+
+
+/*********/
+
+const color = 'red';
+
+switch(color){
+    case 'green':
+        console.log('green')
+        break;
+    case 'red':
+        console.log('red')
+        break;
+    case 'yellow':
+        console.log('yellow')
+        break;
+}
+
+/*****//*/ */
+
